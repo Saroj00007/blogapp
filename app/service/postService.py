45d416fccd0c6fa5cff32f14a1dea121a7174d3c
Saroj00007 <saroj.kandel5856@gmail.com>
2026-09-08 18:48:34 +0545

    implementd the post endponints

---
+++ app/service/postService.py
@@ -0,0 +1,26 @@
+
+from app.db.session import get_session
+from app.schema.post import PostCreate
+
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.models.post import Post
+
+def create_post(
+    post_data : PostCreate , 
+    db: Session  , 
+    author_id : int
+): 
+    post = Post(
+        title = post_data.title , 
+        content = post_data.content , 
+        published = post_data.published , 
+        author_id  = author_id 
+    )
+    
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    
+    return post
+    
